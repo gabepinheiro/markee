b0db81cb112a7133d715eb672a5cec5e8bd216b3
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-09-14 18:20:29 -0300

    add GlobalStyle

--- src/root.tsx
+++ src/root.tsx
@@ -1,13 +1,37 @@
 import { theme } from 'resources/theme'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { App } from './app'
 
 function Root () {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+  }
+`
 
 export { Root }
