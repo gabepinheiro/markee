b7dc364c5a4e7fe11846ba6cb6aa5d9e43301188
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-09-15 16:25:37 -0300

    add title, button and styles on sidebar

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,10 +1,16 @@
 import styled, { css } from 'styled-components/macro'
-import { Logo } from 'ui/icons'
+import { ButtonFullWidth } from 'ui/button'
+import { Logo, PlusIcon } from 'ui/icons'
 
 export function Sidebar () {
   return (
     <Wrapper>
       <Logo />
+      <Title>Arquivos</Title>
+      <ButtonFullWidth>
+        <PlusIcon />
+        Adicionar arquivos
+      </ButtonFullWidth>
     </Wrapper>
   )
 }
@@ -22,6 +28,39 @@ const Wrapper = styled.aside`
     ${Logo} {
       margin-top: 47px;
       margin-bottom: 50px;
+    }
+
+    ${ButtonFullWidth} {
+      margin-bottom: 34px;
+    }
+  `}
+`
+
+const Title = styled.h2`
+  ${({ theme }) => css`
+    width: 100%;
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.bold};
+
+    margin-bottom: 35px;
+
+    display: flex;
+    align-items:center;
+    gap:6px;
+
+    ::before {
+      background: ${theme.colors.primary};
+      content: '';
+      height: 2px;
+      width: 14px;
+    }
+
+    ::after {
+      background: ${theme.colors.primary};
+      content: '';
+      flex-grow: 1;
+      height: 2px;
     }
   `}
 `
