aac5d13bae4ff6f22371d47760c1bac20e2af92e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-09-15 15:35:42 -0300

    extends logo svg and icons svg for styled components

---
+++ src/ui/icons/icons.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro'
+
+import { ReactComponent as LogoSvg } from 'resources/assets/logo.svg'
+import { ReactComponent as PlusSvg } from 'resources/assets/plus-symbol.svg'
+import { ReactComponent as DeleteSvg } from 'resources/assets/delete-icon.svg'
+import { ReactComponent as FileSvg } from 'resources/assets/file-white-icon.svg'
+import { ReactComponent as EditingSvg } from 'resources/assets/editing-icon.svg'
+import { ReactComponent as SavingSvg } from 'resources/assets/saving-icon.svg'
+import { ReactComponent as SavedSvg } from 'resources/assets/saved-icon.svg'
+
+export const Logo = styled(LogoSvg)``
+export const PlusIcon = styled(PlusSvg)``
+export const DeleteIcon = styled(DeleteSvg)``
+export const FileIcon = styled(FileSvg)``
+export const EditingIcon = styled(EditingSvg)``
+export const SavingIcon = styled(SavingSvg)``
+export const SavedIcon = styled(SavedSvg)``
