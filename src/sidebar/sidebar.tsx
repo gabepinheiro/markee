b8dc60457a3297820247fee5ef5ca16e08da006e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-09-15 16:20:35 -0300

    add logo on sidebar and styles

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,9 +1,10 @@
 import styled, { css } from 'styled-components/macro'
+import { Logo } from 'ui/icons'
 
 export function Sidebar () {
   return (
     <Wrapper>
-      <h1>Markee app</h1>
+      <Logo />
     </Wrapper>
   )
 }
@@ -11,5 +12,16 @@ export function Sidebar () {
 const Wrapper = styled.aside`
   ${({ theme }) => css`
     background: ${theme.colors.black};
+
+    padding: 0 32px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ${Logo} {
+      margin-top: 47px;
+      margin-bottom: 50px;
+    }
   `}
 `
