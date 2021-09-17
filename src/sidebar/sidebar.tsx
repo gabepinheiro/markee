a6246cedda5e1b643ebc2f594349a1f67143d6b7
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-09-17 15:00:28 -0300

    remove static state and add useState on <Sidebar />

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react'
 import { ButtonFullWidth } from 'ui/button'
 import { Logo, PlusIcon } from 'ui/icons'
+import { FileProps } from './file-item/types'
 import { ListFile } from './list-file'
 
 import * as S from './styles'
 
 export function Sidebar () {
+  const [files, setFiles] = useState<FileProps[]>([{
+    id: '1',
+    name: 'README.md',
+    content: '## Bootcamp React',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: '2',
+    name: 'CONTRIBUTING.md',
+    content: '## Como contribuir',
+    active: true,
+    status: 'saved',
+  }])
+
   return (
     <S.Wrapper>
       <Logo />
@@ -16,7 +33,7 @@ export function Sidebar () {
         Adicionar arquivos
       </ButtonFullWidth>
 
-      <ListFile />
+      <ListFile files={files} />
     </S.Wrapper>
   )
 }
