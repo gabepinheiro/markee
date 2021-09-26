baf5de296a3afe70651bfb83155f9e6f52d0dc3c
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-09-26 15:37:01 -0300

    applies line breaks to <S.MarkdownPreview /> content

--- src/content/styles.ts
+++ src/content/styles.ts
@@ -78,12 +78,18 @@ export const MarkdownPreview = styled.output`
     height: 95%;
     overflow: auto;
 
+    overflow-wrap: break-word;
+
     &::-webkit-scrollbar {
       all: unset;
     }
 
     h1, h2, h3, h4, h5, h6 {
       margin-top: 0;
+    }
+
+    pre {
+      white-space: pre-wrap;
     }
 
     a:link,
